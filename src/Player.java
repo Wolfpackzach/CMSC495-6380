821fe39991e9be78a4f87b16db1cc33ebe243a7c
Stephen <Stephen@stephens-macbook-pro.local>
2018-09-30 16:16:40 -0400

    added to description

--- src/Player.java
+++ src/Player.java
@@ -1,67 +1,61 @@
-
 /*
  * File Name:		Player.java
  * Class:		CMSC 495-6380
  * Group:		Group 6
- * Authors:		Will Krohn, Stephen
+ * Authors:		Will Krohn, Stephen Parsons
  * Date: 		9/29/2018
- * Purpose: 		This document is responsible for the Player java class for group 6's project to predict wins
+ * Purpose:  This document is responsible for the Player java class for group 6's project to predict wins for a season of a 1v1 sport. Contains name, rank, and win/loss count.
  */
 
 import java.util.*;
 
-
 public class Player {
-	String name;
-	int winCount, lossCount, rank;
-	
-	Player(Scanner sc){
-		if (sc.hasNext()){
-			name = sc.next();
-		}
-		if (sc.hasNext()){
-			rank = sc.nextInt();
-		}	
-	
-	}
-	
+  String name;
+  int winCount, lossCount, rank;
 
-	int getRank(){
-		return rank;
-	}
+  Player(Scanner sc) {
+    if (sc.hasNext()) {
+      name = sc.next();
+    }
+    if (sc.hasNext()) {
+      rank = sc.nextInt();
+    }
+  }
 
-	void setRank(int rank){
-		this.rank = rank;
-	}
+  int getRank() {
+    return rank;
+  }
 
-	void setwinCount(int winCount){
-		this.winCount = winCount;
-	}
+  void setRank(int rank) {
+    this.rank = rank;
+  }
 
-	void setlossCount
-	(int lossCount){
-		this.lossCount = lossCount;
+  void setwinCount(int winCount) {
+    this.winCount = winCount;
+  }
 
-	}
+  void setlossCount(int lossCount) {
+    this.lossCount = lossCount;
+  }
 
-	int getwinCount(){
-		return winCount;
-	}
-	int getlossCount
-	(){
-		return lossCount;
+  int getwinCount() {
+    return winCount;
+  }
 
-	}
-	void resetwinCountlossCount
-	(){
-		winCount = 0;
-		lossCount  = 0;
-	}
-	String getName(){
-		return name;
-	}
-	void setName(String newName){
-		newName = name;
-	}
+  int getlossCount() {
+    return lossCount;
+  }
 
+  void resetwinCountlossCount() {
+    winCount = 0;
+    lossCount = 0;
+  }
+
+  String getName() {
+    return name;
+  }
+
+  void setName(String newName) {
+    newName = name;
+  }
 }
